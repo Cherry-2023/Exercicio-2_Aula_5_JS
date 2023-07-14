05e7853e11fcb32919c01ba80caa0dab02abf91b
Cherry-2023 <Cherry5466@gmail.com>
2023-07-14 13:21:27 -0300

    Subiendo Exercicio-2 Aula-5 Javascript

---
+++ Exercicio_2_JS.js
@@ -0,0 +1,23 @@
+let ptnome = prompt('Digite o Nome da primeira pessoa: ')
+let ptsobrenome = prompt('Digite o Sobrenome da primeira pessoa')
+let ptidade = prompt('Digite o Idade da primeira pessoa')
+let ptestatura = prompt('Digite o Estatura da primeira pessoa')
+let ptcor = prompt('Digite o Cor favorito da primeira pessoa')
+let ptnome2 = prompt('Digite o Nome da segunda pessoa: ')
+let ptsobrenome2 = prompt('Digite o Sobrenome da segunda pessoa')
+let ptidade2 = prompt('Digite o Idade da segunda pessoa')
+let ptestatura2 = prompt('Digite o Estatura da segunda pessoa')
+let ptcor2 = prompt('Digite o Cor favorito da segunda pessoa')
+Number(ptidade)
+Number(ptestatura)
+if (ptidade > ptidade2) {
+    console.log(`${ptnome} é mais velho/a que ${ptnome2}`)
+} else {
+    console.log(`${ptnome2} é mais velho/a que ${ptnome}`)
+};
+if (ptestatura > ptestatura2) {
+    console.log(`${ptnome} é mais Alto/a que ${ptnome2}`)
+} else {
+    console.log(`${ptnome2} é mais Alto/a que ${ptnome}`)
+};
+console.log(`${ptnome} ${ptsobrenome} gosta do Cor ${ptcor} e ${ptnome2} ${ptsobrenome2} gosta de Cor ${ptcor2}`);
